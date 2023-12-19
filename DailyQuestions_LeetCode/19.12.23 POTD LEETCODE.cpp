57918e6c41626d1f98eed9d15675d62515973bf1
Pragadeeshwar-D <118243568+Pragadeeshwar-D@users.noreply.github.com>
2023-12-19 18:14:23 +0530

    Add files via upload

---
+++ DailyQuestions_LeetCode/19.12.23 POTD LEETCODE.cpp
@@ -0,0 +1,78 @@
+/*661. Image Smoother
+
+An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down the average of the cell and the eight surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, we do not consider it in the average (i.e., the average of the four cells in the red smoother).
+
+
+Given an m x n integer matrix img representing the grayscale of an image, return the image after applying the smoother on each cell of it.
+
+ 
+
+Example 1:
+
+
+Input: img = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[0,0,0],[0,0,0],[0,0,0]]
+Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+For the points (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+Example 2:
+
+
+Input: img = [[100,200,100],[200,50,200],[100,200,100]]
+Output: [[137,141,137],[141,138,141],[137,141,137]]
+Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor((100+200+200+50)/4) = floor(137.5) = 137
+For the points (0,1), (1,0), (1,2), (2,1): floor((200+200+50+200+100+100)/6) = floor(141.666667) = 141
+For the point (1,1): floor((50+200+200+200+200+100+100+100+100)/9) = floor(138.888889) = 138
+*/
+
+
+SOLUTION :
+
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int[][] ans = new int[img.length][img[0].length];
+        int sum, count;
+        for (int i = 0; i < img.length; i++) {
+            for (int j = 0; j < img[0].length; j++) {
+                sum = img[i][j];
+                count = 1;
+                if ((j - 1) >= 0) {
+                    sum += img[i][j - 1];
+                    count++;
+                }
+                if ((j + 1) < img[0].length) {
+                    sum += img[i][j + 1];
+                    count++;
+                }
+                if ((i - 1) >= 0) {
+                    sum += img[i - 1][j];
+                    count++;
+                    if ((j - 1) >= 0) {
+                        sum += img[i - 1][j - 1];
+                        count++;
+                    }
+                    if ((j + 1) < img[0].length) {
+                        sum += img[i - 1][j + 1];
+                        count++;
+                    }
+                }
+                if ((i + 1) < img.length) {
+                    sum += img[i + 1][j];
+                    count++;
+                    if ((j - 1) >= 0) {
+                        sum += img[i + 1][j - 1];
+                        count++;
+                    }
+                    if ((j + 1) < img[0].length) {
+                        sum += img[i + 1][j + 1];
+                        count++;
+                    }
+                }
+                ans[i][j] = sum / count;
+            }
+        }
+        return ans;
+    }
+}
